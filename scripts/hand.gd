0810c921f9d98031822fd235acf3adec20e6e8b3
XJIEI5 <egorchernov37@yandex.ru>
2025-10-10 21:53:56 +0300

    hand carrying item now has type

--- scripts/hand.gd
+++ scripts/hand.gd
@@ -1,8 +1,23 @@
 extends Node
 
 
+enum ItemType {
+	Nothing,
+	Cup,
+}
+
 var CarriedItem: Node
+# WARNING: ONLY ItemType EXPECTED
+var itemType = ItemType.Nothing
 
 func TakeNewItem(item: Node):
 	self.CarriedItem = item
-	print("CARRYING %s" % item.to_string())
+	itemType = assignType(item)
+	print("CARRYING %s typeof %s" % [item.to_string(), ItemType.keys()[itemType]])
+
+
+# WARNING: RETURNS ItemType
+func assignType(item: Node):
+	if item is Coffee.Cup:
+		return ItemType.Cup
+	push_error("hand can't carry item from node %s" % item)
